9e4336a49a25ae8a75e3ebd4e9872ea1f66915d4
ardaozkaranfil <arda.ozkaranfil0@gmail.com>
2026-04-25 22:27:39 +0300

    initial commit
    HashTable class skeleton with constructor, getters/setters and empty method stubs

---
+++ Hash Table/src/HashTable.java
@@ -0,0 +1,113 @@
+public class HashTable
+{
+    private int capacity;
+    private int elementSize;
+    private Integer[] table;
+
+    public HashTable(int capacity) {
+        this.capacity = capacity;
+        this.elementSize = 0;
+        this.table = new Integer[capacity];
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getElementSize() {
+        return elementSize;
+    }
+
+    public void setElementSize(int elementSize) {
+        this.elementSize = elementSize;
+    }
+
+    public Integer[] getTable() {
+        return table;
+    }
+
+    public void setTable(Integer[] table) {
+        this.table = table;
+    }
+
+    public void printTable()
+    {
+        for (int ind = 0; ind < getCapacity(); ind++)
+        {
+            if (getTable()[ind] != null) {
+                System.out.println(ind + ": |" + getTable()[ind] + "|");
+            }
+            else
+            {
+                System.out.println(ind + ": |  |");
+            }
+        }
+    }
+
+    private boolean isPrime(int n)
+    {
+        if (n <= 1)
+            return false;
+        if (n <= 3)
+            return true;
+
+        if (n % 2 == 0 || n % 3 == 0)
+            return false;
+
+        for (int i = 5; i * i <= n; i = i + 6)
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
+
+        return true;
+    }
+
+    private int nextPrime(int N)
+    {
+        if (N <= 1)
+            return 2;
+
+        int prime = N*2;
+        boolean found = false;
+
+        while (!found)
+        {
+            prime++;
+
+            if (isPrime(prime))
+                found = true;
+        }
+
+        return prime;
+    }
+
+    protected Integer hashx(Integer x)
+    {
+        return x;
+    }
+
+    protected void rehash()
+    {
+
+    }
+
+    public void insert(Integer x)
+    {
+
+    }
+
+    public Boolean contains(Integer value)
+    {
+
+        return null;
+    }
+
+    public Integer getValue(int key)
+    {
+
+        return null;
+    }
+}

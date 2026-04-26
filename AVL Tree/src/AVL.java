0264c8ee5de7d399527cc68095d0a2feb61112cc
ardaozkaranfil <arda.ozkaranfil0@gmail.com>
2026-04-26 16:50:17 +0300

    initial commit
    AVL class skeleton with AVLNode class, constructors, getHeight helper and empty method stubs.

---
+++ AVL Tree/src/AVL.java
@@ -0,0 +1,67 @@
+class AVLNode{
+
+    int elem, height;
+    AVLNode left, right;
+
+    AVLNode(int elem, AVLNode leftTree, AVLNode rightTree){
+        this.elem = elem;
+        height = 0;
+        left = leftTree;
+        right = rightTree;
+    }
+}
+
+public class AVL {
+
+    AVLNode root;
+
+    public AVL(AVLNode root){
+        this.root = root;
+    }
+
+    public AVL(){
+        this.root = null;
+    }
+
+    private int getHeight(AVLNode node){
+        if (node == null)
+            return -1;
+        return node.height;
+    }
+
+    private AVLNode rotateLL(AVLNode root){
+        return null;
+    }
+
+    private AVLNode rotateRR(AVLNode root){
+        return null;
+    }
+
+    private AVLNode rotateLR(AVLNode root){
+        return null;
+    }
+
+    private AVLNode rotateRL(AVLNode root){
+        return null;
+    }
+
+    public AVLNode insert(int elem, AVLNode root){
+        return null;
+    }
+
+    public AVLNode balance(AVLNode root){
+        return null;
+    }
+
+    public AVLNode search(int elem, AVLNode root){
+        return null;
+    }
+
+    public Boolean isAVL(AVLNode root){
+        return null;
+    }
+
+    public static Boolean AVLEquals(AVLNode root1, AVLNode root2){
+        return null;
+    }
+}

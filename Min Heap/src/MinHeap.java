0d50c27405a24a1d70adf1e6c365eb46027002da
ardaozkaranfil <arda.ozkaranfil0@gmail.com>
2026-04-29 23:50:26 +0300

    implement percolateUp and percolateDown methods
    percolateUp places the element at the correct position by moving it towards the root, percolateDown restores heap property by shifting an element down to its correct position handling both single and double child cases.

--- Min Heap/src/MinHeap.java
+++ Min Heap/src/MinHeap.java
@@ -62,11 +62,56 @@ public class MinHeap {
     }
 
     private void percolateUp(int x, int hole){
-        return;
+        if(array.length == 0 || x >= array[hole / 2]){
+            array[hole] = x;
+        }
+        else {
+            for (int i = hole; i > 1; i = i / 2) {
+                if (x < array[i / 2]) {
+                    array[i] = array[i / 2];
+                    array[i / 2] = x;
+                } else {
+                    break;
+                }
+            }
+        }
     }
 
     private void percolateDown(int hole){
-        return;
+        int temp;
+
+        for(int i = hole; i * 2 < currentSize || i * 2 == currentSize; ){
+            if(i * 2 == currentSize && array[i] > array[i*2]){
+                temp = array[i * 2];
+                array[i * 2] = array[i];
+                array[i] = temp;
+
+                i = i * 2;
+            }
+            else if(array[i] <= array[i*2] && i * 2 == currentSize){
+                break;
+            }
+            else {
+
+                int minChild = Math.min(array[i * 2], array[(i * 2) + 1]);
+
+                if (minChild == array[i * 2] && array[i] > minChild) {
+                    temp = array[i * 2];
+                    array[i * 2] = array[i];
+                    array[i] = temp;
+
+                    i = i * 2;
+                } else if (minChild == array[(i * 2) + 1] && array[i] > minChild) {
+                    temp = array[(i * 2) + 1];
+                    array[(i * 2) + 1] = array[i];
+                    array[i] = temp;
+
+                    i = (i * 2) + 1;
+                } else {
+                    break;
+                }
+            }
+        }
     }
 
     public int deleteMin(){

1f26d3c031c2410023f9c93cf2293637c15fff09
ardaozkaranfil <arda.ozkaranfil0@gmail.com>
2026-05-03 21:54:12 +0300

    implement bubbleSort method
    Sorts the given array in ascending order using bubble sort. Iterates up to arr.length - 1 passes, comparing and swapping adjacent elements. Exits early if no swaps occur during a pass, indicating the array is already sorted.

--- Sorting/src/Sorting.java
+++ Sorting/src/Sorting.java
@@ -1,7 +1,26 @@
 public class Sorting {
 
     public static void bubbleSort(int[] arr){
-        return;
+
+
+        for(int j = 0; j < arr.length - 1; j++) {
+            boolean sorted = false;
+
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i - 1] > arr[i]) {
+                    int temp = arr[i - 1];
+
+                    arr[i - 1] = arr[i];
+                    arr[i] = temp;
+
+                    sorted = true;
+                }
+            }
+
+            if(!sorted){
+                break;
+            }
+        }
     }
 
     public static void heapSort(int[] arr){
